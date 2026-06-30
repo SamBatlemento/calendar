822f048e4e9ac885faff285e4b94857007d63c29
SamBatlemento <sa213300@ucf.edu>
2026-06-30 18:21:08 -0400

    Update CardUI.tsx
    test 3

--- frontend/src/components/CardUI.tsx
+++ frontend/src/components/CardUI.tsx
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 function CardUI()
 {
 
-    //testing edit function 2
+    //testing edit function 3
     
     let _ud : any = localStorage.getItem('user_data');
     let ud = JSON.parse(_ud);
