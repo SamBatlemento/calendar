ddd87bf762cf7cd2503cf9d9f8c33215f65958d8
Sam Batlemento <srbatlemento@gmail.com>
2026-07-01 23:16:29 -0400

    Fixed hard-coded name in LoggedInName.tsx

--- frontend/src/components/LoggedInName.tsx
+++ frontend/src/components/LoggedInName.tsx
@@ -1,19 +1,20 @@
 function LoggedInName()
 {
 
-    //var user={}
+    const ud = JSON.parse(localStorage.getItem('user_data') || '{}');
 
     function doLogout(event:any) : void
     {
         event.preventDefault();
 
         localStorage.removeItem("user_data")
+        localStorage.removeItem('token_data');
         window.location.href = '/';
     };
 
     return(
         <div id="loggedInDiv">
-            <span id="userName">Logged In As John Doe </span><br />
+            <span id="userName">Logged In As {ud.firstName} {ud.lastName} </span><br />
             <button type="button" id="logoutButton" className="buttons" onClick={doLogout}> Log Out </button>
         </div>
     );
