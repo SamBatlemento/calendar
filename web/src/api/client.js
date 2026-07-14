dbc5ab4d076f88200a7e23a46b7f70b4a25984eb
Sam Batlemento <srbatlemento@gmail.com>
2026-07-13 21:58:48 -0400

    Stopped the 401 interceptor from eating login errors

--- web/src/api/client.js
+++ web/src/api/client.js
@@ -15,8 +15,10 @@ client.interceptors.request.use((config) => {
 client.interceptors.response.use(
   (res) => res,
   (err) => {
-    if (err.response?.status === 401) {
+    const isLoginRequest = err.config?.url?.includes('/login');
+    if (err.response?.status === 401 && !isLoginRequest) {
       localStorage.removeItem('token');
+      localStorage.removeItem('user');
       window.location.href = '/login';
     }
     return Promise.reject(err);
