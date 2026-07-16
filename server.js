ba129bca528a3e7915b5a0fe105bc5771cdf60fc
User4872 <wangyin011@gmail.com>
2026-07-16 13:54:35 -0400

    Fix

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('MongoDB connected');
     app.listen(process.env.PORT || 5000, () =>
-    console.log('Server running on port ${rocess.env.PORT || 5000}'));
+    console.log('Server running on port ${process.env.PORT || 5000}'));
   })
   .catch(err => {
     console.error('MongoDB connection failed:', err);
