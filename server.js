942cbb72d20a3842a37e172e9ebc45b88d3ba015
Sam Batlemento <srbatlemento@gmail.com>
2026-07-20 02:50:26 -0400

    Fixed log bug and improved readability

--- server.js
+++ server.js
@@ -23,17 +23,6 @@ app.use('/api/refresh', authLimiter);
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(process.env.PORT || 5000, () =>
-    console.log('Server running on port ${process.env.PORT || 5000}'));
-  })
-  .catch(err => {
-    console.error('MongoDB connection failed:', err);
-    process.exit(1);
-  });
-  
 const accountApi = require('./API/AccountApi.js');
 const passwordApi = require('./API/PasswordApi.js');
 const memberApi = require('./API/MemberApi.js');
@@ -49,3 +38,14 @@ exercisesApi.setApp(app, mongoose);
 assignmentsApi.setApp(app, mongoose);
 mealApi.setApp(app, mongoose);
 gameApi.setApp(app, mongoose);
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT || 5000, () =>
+    console.log(`Server running on port ${process.env.PORT || 5000}`));
+  })
+  .catch(err => {
+    console.error('MongoDB connection failed:', err);
+    process.exit(1);
+  });
